53549afa59fd7098cdadf17e8e965d3834387429
Theani33 <112559622+Theani33@users.noreply.github.com>
2023-05-02 23:30:46 +0200

    Update tcp_server.go

--- tcp_server.go
+++ tcp_server.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+
+	"fmt"
+	"github.com/Theani33/funtemps/conv"
 	"io"
 	"log"
 	"net"
+	"strconv"
+	"strings"
 	"sync"
+
+	"github.com/Theani33/is105sem03/mycrypt"
 )
 
 func main() {
 
 	var wg sync.WaitGroup
 
-	server, err := net.Listen("tcp", "172.17.0.4:8080")
+	server, err := net.Listen("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,9 +43,30 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
+dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
+switch msg := string(dekryptertMelding); msg {
+                                        case "ping":
+
+							response1 := "pong"
+							     encryptedResponse := mycrypt.Krypter([]rune(response1), mycrypt.ALF_SEM03, 4)
+							_,err=c.Write([]byte(string(encryptedResponse)))
+                                         case "Kjevik;SN39040;18.03.2022 01:50;6":
+                                  		parts := strings.Split(msg, ";")
+
+				            	  if len(parts) < 4 {
+							log.Println("Invalid input message")
+							return
+						               }
+                     				t, err := strconv.ParseFloat(parts[len(parts)-1], 64)
+                     					if err != nil {
+                 				        log.Println(err) }
+
+				               f := conv.CelsiusToFarhenheit(t)
+						response:= fmt.Sprintf("Kjevik;SN39040;18.03.2022 01:50;%0.2f", f)
+						     encryptedResponse := mycrypt.Krypter([]rune(response), mycrypt.ALF_SEM03, 4)
+							_,err = c.Write([]byte(string(encryptedResponse)))
 					default:
 						_, err = c.Write(buf[:n])
 					}
